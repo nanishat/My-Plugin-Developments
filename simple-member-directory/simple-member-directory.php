2c0d31c7c6282335af027512405c9cefa38ee150
Md Safiul Mujnebin <spy.spid3y@gmail.com>
2025-07-17 14:44:38 +0600

    plugin info setup

---
+++ simple-member-directory/simple-member-directory.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:         Simple Member Directory
+ * Plugin URI:          https://wordpress.com/plugins/simple-member-directory/
+ * Description:         A simple member directory for your website where each member can be associated with multiple teams.
+ * Version:             1.0.0
+ * Requires at least:   5.2
+ * Requires PHP:        7.4
+ * Author:              Safiul Mujnebin
+ * Author URI: 
+ * License:             GPLv2 or later
+ * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:          https://github.com/nanishat
+ * Text Domain:         smd
+ */
+
+?>
