a3155a8805ef8854067387ab7e36d488c67a0983
nanishat <spy.spid3y@gmail.com>
2025-07-30 13:53:28 +0600

    function: add and display members

--- simple-member-directory/simple-member-directory.php
+++ simple-member-directory/simple-member-directory.php
@@ -7,11 +7,71 @@
  * Requires at least:   5.2
  * Requires PHP:        7.4
  * Author:              Safiul Mujnebin
- * Author URI: 
  * License:             GPLv2 or later
  * License URI:         https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:          https://github.com/nanishat
  * Text Domain:         smd
  */
 
+// Register a shortcode: [simple_member_directory]
+add_shortcode('simple_member_directory', 'smd_display_members');
+
+function smd_display_members() {
+    $args = array(
+        'post_type' => 'smd_member',
+        'post_status' => 'publish',
+        'posts_per_page' => -1
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) {
+        $output = '<div class="smd-member-directory">';
+
+        while ($query->have_posts()) {
+            $query->the_post();
+            $output .= '<div class="smd-member">';
+            $output .= '<h3>' . get_the_title() . '</h3>';
+            $output .= '<p>' . get_the_excerpt() . '</p>';
+            if (has_post_thumbnail()) {
+                $output .= get_the_post_thumbnail(get_the_ID(), 'thumbnail');
+            }
+            $output .= '</div>';
+        }
+
+        $output .= '</div>';
+        wp_reset_postdata();
+    } else {
+        $output = '<p>No members found.</p>';
+    }
+
+    return $output;
+}
+
+
+function smd_register_member_post_type() {
+    $labels = array(
+        'name' => 'Members',
+        'singular_name' => 'Member',
+        'menu_name' => 'Members',
+        'add_new' => 'Add New',
+        'add_new_item' => 'Add New Member',
+        'new_item' => 'New Member',
+        'edit_item' => 'Edit Member',
+        'view_item' => 'View Member',
+        'all_items' => 'All Members',
+        'search_items' => 'Search Members',
+    );
+
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => false,
+        'menu_icon' => 'dashicons-groups',
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'show_in_rest' => true,
+    );
+
+    register_post_type('smd_member', $args);
+}
+add_action('init', 'smd_register_member_post_type');
 ?>
